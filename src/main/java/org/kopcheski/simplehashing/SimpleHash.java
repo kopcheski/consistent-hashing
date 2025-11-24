f6d7145817538c45107481e74518d58058b94909
Vinicius Kopcheski <viniciusfk@gmail.com>
2025-11-24 21:57:39 +0100

    simple hashing with initial test

--- src/main/java/org/kopcheski/simplehashing/SimpleHash.java
+++ src/main/java/org/kopcheski/simplehashing/SimpleHash.java
@@ -1,10 +1,29 @@
 package org.kopcheski.simplehashing;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class SimpleHash {
 
+	private final Map<Integer, List<String>> dataServers = new HashMap<>();
+
+	private final int serverCount;
+
 	public SimpleHash(int servers) {
+		this.serverCount = servers;
 	}
 
 	public void store(String data) {
+		int key = Math.abs(data.hashCode()) % serverCount;
+		dataServers.computeIfAbsent(key, k -> new ArrayList<>()).add(data);
+	}
+
+	public List<Integer> dataCountPerServer() {
+		return dataServers.values()
+				.stream()
+				.map(List::size)
+				.toList();
 	}
 }
--- src/test/java/org/kopcheski/simplehashing/SimpleHashTest.java
+++ src/test/java/org/kopcheski/simplehashing/SimpleHashTest.java
@@ -2,15 +2,40 @@ package org.kopcheski.simplehashing;
 
 import org.junit.jupiter.api.Test;
 
+import java.nio.charset.StandardCharsets;
+import java.security.SecureRandom;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 class SimpleHashTest {
+
+	private static final int SERVERS = 5;
+	private static final int ENTRIES_COUNT = 10_000;
+	private static final double TOLERANCE_PCT = .05;
 
 	@Test
 	void testSimpleHash() {
-		var simpleHash = new SimpleHash(5);
+		var simpleHash = new SimpleHash(SERVERS);
 
-		for (int i = 0; i < 10_000; i++) {
-			simpleHash.store("");
+		for (int i = 0; i < ENTRIES_COUNT; i++) {
+			byte[] array = new byte[7];
+			new SecureRandom().nextBytes(array);
+			var generatedString = new String(array, StandardCharsets.UTF_8);
+
+			simpleHash.store(generatedString);
 		}
+
+		List<Integer> dataCount = simpleHash.dataCountPerServer();
+		assertEquals(SERVERS, dataCount.size());
+		dataCount.forEach(entry -> assertTrue(approximation(entry)));
+	}
+
+	private boolean approximation(Integer value) {
+		int avg = ENTRIES_COUNT / SERVERS;
+		int max = (int) (avg * (1 + TOLERANCE_PCT));
+		int min = (int) (avg * (1 - TOLERANCE_PCT));
+		return value <= max && value >= min;
 	}
 
 }
