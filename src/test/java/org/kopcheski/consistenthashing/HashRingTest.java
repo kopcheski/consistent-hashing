6edfa3503000fd6ed6d1775f66921819f719cdc8
Vinicius Kopcheski <viniciusfk@gmail.com>
2025-12-20 17:21:33 +0100

    more test to add node to HashRing

--- src/test/java/org/kopcheski/consistenthashing/HashRingTest.java
+++ src/test/java/org/kopcheski/consistenthashing/HashRingTest.java
@@ -3,8 +3,7 @@ package org.kopcheski.consistenthashing;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class HashRingTest {
 
@@ -25,6 +24,16 @@ class HashRingTest {
 
 		assertTrue(hashRing.isNodePresent(serverA));
 		assertTrue(hashRing.isNodePresent(serverB));
+	}
+
+	@Test
+	void testReplicasPresentInTheRing() {
+		String serverA = "A";
+		int replicas = 3;
+
+		hashRing.addNode(serverA, replicas);
+
+		assertEquals(replicas + 1, hashRing.nodesCount(), "A total # of replicas + 1 server should be present in the ring.");
 	}
 
 	@Test
