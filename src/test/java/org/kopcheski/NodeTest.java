3fef448d7b1b50a6e5dc3df0acff6304845b5709
Vinicius Kopcheski <viniciusfk@gmail.com>
2025-12-31 12:14:44 +0100

    refactors NodeTest

--- src/test/java/org/kopcheski/NodeTest.java
+++ src/test/java/org/kopcheski/NodeTest.java
@@ -1,6 +1,7 @@
 package org.kopcheski;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class NodeTest {
 
 	private static final String KEY = "key";
-
+	
 	private Node storageNode;
 
 	@BeforeEach
@@ -16,37 +17,58 @@ class NodeTest {
 		this.storageNode = new Node("node-1");
 	}
 
-	@Test
-	void testAddedValueIsStored() {
-		this.storageNode.add(KEY, "value");
-		assertEquals("value", this.storageNode.readValue(KEY));
-	}
+	@Nested
+	class GivenAValueBeingAdded {
 
-	@Test
-	void testAlreadyPresentValueIsOverwritten() {
-		this.storageNode.add(KEY, "value");
-		this.storageNode.add(KEY, "newValue");
-		assertEquals("newValue", this.storageNode.readValue(KEY));
-	}
-
-	@Test
-	void testStorageStaysAtIsMaximumSizeWhenAddingMoreThanItSupports() {
-		for (int i = 0; i <= Node.SIZE + 2; i++) {
-			this.storageNode.add(KEY + i, "value" + i);
+		@Test
+		void thenItIsStored() {
+			storageNode.add(KEY, "value");
+			assertEquals("value", storageNode.readValue(KEY));
 		}
 
-		assertEquals(Node.SIZE, this.storageNode.dumpData().size());
+		@Test
+		void whenInAKeyThatIsAlreadyPresent_thenTheOldValueIsOverwritten() {
+			storageNode.add(KEY, "value");
+			storageNode.add(KEY, "newValue");
+			assertEquals("newValue", storageNode.readValue(KEY));
+		}
+
+		@Test
+		void whenTheStorageIsFull_thenItDropAnyValueToFitTheNewOne() {
+			for (int i = 0; i <= Node.SIZE + 2; i++) {
+				storageNode.add(KEY + i, "value" + i);
+			}
+
+			assertEquals(Node.SIZE, storageNode.dumpData().size());
+		}
+
 	}
 
-	@Test
-	void testRemoveExistingValue() {
-		this.storageNode.add(KEY, "value");
-		this.storageNode.removeValue(KEY);
-		assertThrows(IllegalStateException.class, () -> this.storageNode.readValue(KEY));
+	@Nested
+	class GivenAValueBeingRead {
+
+		@Test
+		void whenItDoesNotExist_thenItFails() {
+			assertThrows(IllegalStateException.class, () -> storageNode.readValue(KEY));
+		}
+
 	}
 
-	@Test
-	void testRemoveNonExistingValueWorksSilently() {
-		assertDoesNotThrow(() -> this.storageNode.removeValue(KEY));
+	@Nested
+	class GivenAValueBeingRemoved {
+
+		@Test
+		void whenItExists_thenItIsRemoved() {
+			storageNode.add(KEY, "value");
+			storageNode.removeValue(KEY);
+			assertThrows(IllegalStateException.class, () -> storageNode.readValue(KEY));
+		}
+
+		@Test
+		void whenItDoesNotExist_thenItWorksSilently() {
+			assertDoesNotThrow(() -> storageNode.removeValue(KEY));
+		}
+
 	}
+
 }
