095af477264f7ed2c9fdffa459db09d2dcbf5903
Vinicius Kopcheski <viniciusfk@gmail.com>
2026-01-25 17:45:35 +0100

    removed unused method

--- src/main/java/org/kopcheski/consistenthashing/model/Key.java
+++ src/main/java/org/kopcheski/consistenthashing/model/Key.java
@@ -3,12 +3,7 @@ package org.kopcheski.consistenthashing.model;
 /**
  * A Key that maps a {@link org.kopcheski.consistenthashing.model.Value} to a {@link org.kopcheski.consistenthashing.Node}
  */
-public record Key(String value) implements RingValue, Hashable, Comparable<Key> {
-
-	@Override
-	public int compareTo(Key other) {
-		return this.value.compareTo(other.value);
-	}
+public record Key(String value) implements RingValue, Hashable {
 
 	@Override
 	public String value() {
