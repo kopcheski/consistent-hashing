65c39313dd3456b551d863dd4740c93e3c933bc6
Vinicius Kopcheski <viniciusfk@gmail.com>
2026-02-16 18:04:53 +0100

    more readable ComponentTest

--- src/test/java/org/kopcheski/consistenthashing/ComponentTest.java
+++ src/test/java/org/kopcheski/consistenthashing/ComponentTest.java
@@ -3,14 +3,15 @@ package org.kopcheski.consistenthashing;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 class ComponentTest {
 
 	@Test
 	void test() {
-		var client = new Client(Map.of("A", new Node("A"), "B", new Node("B"), "C", new Node("C")));
-		IntStream.range(0, 10_000).forEach(i -> {
+		var client = new Client(nodes(150));
+		IntStream.range(0, 40_000).forEach(i -> {
 			var key = "key_" + i;
 			var value = "value_" + i;
 			client.put(key, value);
@@ -19,4 +20,11 @@ class ComponentTest {
 		client.dumpCountPerNode();
 
 	}
+
+	private Map<String, Node> nodes(int count) {
+		return IntStream.range(0, count)
+				.mapToObj(i -> "node_" + i)
+				.collect(Collectors.toMap(id -> id, Node::new));
+	}
+
 }
